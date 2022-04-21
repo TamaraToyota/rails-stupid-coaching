94afcc978fd398875aada38ad67d9d8848be1be0
tamarazoppellari <tamistoyota@gmail.com>
2022-04-21 19:13:19 -0300

    solved challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,7 +12,7 @@ class QuestionsController < ApplicationController
     elsif @question =~ /i am going to work/i
       @answer = "Great!"
     elsif @question.ends_with?("?")
-      @answer = "Silly question, get dressed and go to work!"
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
